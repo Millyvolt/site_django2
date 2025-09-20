c25f141ee611f695a7415b0ec2552dde79be52d0
Vladimir <vovikalin@mail.ru>
2025-09-20 17:45:49 +0300

    Tutorual ends, added my_notes.md

---
+++ my_notes.md
@@ -0,0 +1,14 @@
+
+****Virtual environment****
+
+        To create a virtual environment, decide upon a directory where you want to place it, and run the venv module as a script with the directory path:
+*python -m venv tutorial-env*
+
+    То асtivate on Windows, run:
+*tutorial-env\Scripts\activate*
+
+    To deactivate a virtual environment, type in terminal:
+
+*deactivate*
+
+
--- mysite/settings.py
+++ mysite/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
+import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        "DIRS": [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +123,19 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+TESTING = "test" in sys.argv or "PYTEST_VERSION" in os.environ
+
+if not TESTING:
+    INSTALLED_APPS = [
+        *INSTALLED_APPS,
+        "debug_toolbar",
+    ]
+    MIDDLEWARE = [
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
+        *MIDDLEWARE,
+    ]
